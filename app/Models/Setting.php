1a9cc16c818c0fdee9da6ce65080df78cddd41b4
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-08-24 19:34:32 +0300

    an init

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -77,7 +77,7 @@ class Setting extends Model
 
               // Image
               if(isset($value['base64Image'])) {
-                if($value['base64Image'] && !Str::contains($value['base64Image'], ['uploads'])) {
+                if($value['base64Image'] && !Str::contains($value['base64Image'], ['uploads', 'false'])) {
                   $image = Imageable::uploadImage($value['base64Image']);
                   $row->image()->delete();
                   $row->image()->create(['url' => $image]);
