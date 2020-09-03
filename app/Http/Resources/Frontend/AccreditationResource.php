3c3d0b9d1646850c0be2e240efd5cba8eae7b2b2
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-03 11:28:57 +0300

    an init

--- app/Http/Resources/Frontend/AccreditationResource.php
+++ app/Http/Resources/Frontend/AccreditationResource.php
@@ -14,6 +14,10 @@ class AccreditationResource extends JsonResource
      */
     public function toArray($request)
     {
+        $content[] = ['name' => 'ali'];
+        $content[] = ['name' => 'hema'];
+
+        $content->toArray();
         return [
             'id'            => $this->id,
             'image'         => ($this->image) ? request()->root() . $this->image->url : NULL,
@@ -30,61 +34,60 @@ class AccreditationResource extends JsonResource
             'body4'          => $this->body4,
             'body5'          => $this->body5,
 
-
-            'data' => [
-                {
-                    // d1
-                    'body'        => $this->body1_1,
-                    'background'  => ($this->image1_1) ? request()->root() . $this->image1_1->url : NULL,
+            'con'          => $content,
+            // 'content' => [{
+            //         // d1
+            //         'body'        => $this->body1_1,
+            //         'background'  => ($this->image1_1) ? request()->root() . $this->image1_1->url : NULL,
                     
-                    // d2
-                    'body_left'   => $this->body2_1,
-                    'body_right'  => $this->body2_1_r,
-                    'image'     => ($this->image2_1) ? request()->root() . $this->image2_1->url : NULL,
-                    'label'     => $this->label2_1,
-                    'color'     => $this->color2_1,
-                    'image_dir' => 'right',
-                },
-                {
-                    // d1
-                    'body'   => $this->body1_2,
-                    'image'  => ($this->image1_2) ? request()->root() . $this->image1_2->url : NULL,
+            //         // d2
+            //         'body_left'   => $this->body2_1,
+            //         'body_right'  => $this->body2_1_r,
+            //         'image'     => ($this->image2_1) ? request()->root() . $this->image2_1->url : NULL,
+            //         'label'     => $this->label2_1,
+            //         'color'     => $this->color2_1,
+            //         'image_dir' => 'right',
+            //     },
+            //     {
+            //         // d1
+            //         'body'   => $this->body1_2,
+            //         'image'  => ($this->image1_2) ? request()->root() . $this->image1_2->url : NULL,
                     
-                    // d2
-                    'body_left'   => $this->body2_2,
-                    'body_right'  => $this->body2_2_r,
-                    'image'     => ($this->image2_2) ? request()->root() . $this->image2_2->url : NULL,
-                    'label'     => $this->label2_2,
-                    'color'     => $this->color2_2,
-                    'image_dir' => 'left',
-                },
-                {
-                    // d1
-                    'body'   => $this->body1_3,
-                    'image'  => ($this->image1_3) ? request()->root() . $this->image1_3->url : NULL,
+            //         // d2
+            //         'body_left'   => $this->body2_2,
+            //         'body_right'  => $this->body2_2_r,
+            //         'image'     => ($this->image2_2) ? request()->root() . $this->image2_2->url : NULL,
+            //         'label'     => $this->label2_2,
+            //         'color'     => $this->color2_2,
+            //         'image_dir' => 'left',
+            //     },
+            //     {
+            //         // d1
+            //         'body'   => $this->body1_3,
+            //         'image'  => ($this->image1_3) ? request()->root() . $this->image1_3->url : NULL,
                     
-                    // d2
-                    'body_left'   => $this->body2_3,
-                    'body_right'  => $this->body2_3_r,
-                    'image'      => ($this->image2_3) ? request()->root() . $this->image2_3->url : NULL,
-                    'label'      => $this->label2_3,
-                    'color'      => $this->color2_3,
-                    'image_dir'  => 'right',
-                },
-                {
-                    // d1
-                    'body'   => $this->body1_4,
-                    'image'  => ($this->image1_4) ? request()->root() . $this->image1_4->url : NULL,
+            //         // d2
+            //         'body_left'   => $this->body2_3,
+            //         'body_right'  => $this->body2_3_r,
+            //         'image'      => ($this->image2_3) ? request()->root() . $this->image2_3->url : NULL,
+            //         'label'      => $this->label2_3,
+            //         'color'      => $this->color2_3,
+            //         'image_dir'  => 'right',
+            //     },
+            //     {
+            //         // d1
+            //         'body'   => $this->body1_4,
+            //         'image'  => ($this->image1_4) ? request()->root() . $this->image1_4->url : NULL,
                     
-                    // d2
-                    'body_left'   => $this->body2_4,
-                    'body_right'  => $this->body2_4_r,
-                    'image'     => ($this->image2_4) ? request()->root() . $this->image2_4->url : NULL,
-                    'label'     => $this->label2_4,
-                    'color'     => $this->color2_4,
-                    'image_dir' => 'left',
-                }
-            ],
+            //         // d2
+            //         'body_left'   => $this->body2_4,
+            //         'body_right'  => $this->body2_4_r,
+            //         'image'     => ($this->image2_4) ? request()->root() . $this->image2_4->url : NULL,
+            //         'label'     => $this->label2_4,
+            //         'color'     => $this->color2_4,
+            //         'image_dir' => 'left',
+            //     }
+            // ],
 
 
             'download_name' => $this->download_name ?? NULL,
