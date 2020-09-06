0cb454295b2a46b5d592a8c9007013560d6cb1dc
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-06 17:19:04 +0300

    an init

--- app/Http/Resources/Frontend/EventResource.php
+++ app/Http/Resources/Frontend/EventResource.php
@@ -17,7 +17,11 @@ class EventResource extends JsonResource
         return [
             'id'      => $this->id,
             'image'   => ($this->image) ? request()->root() . $this->image->url : NULL,
-            'body'    => $this->body1,
+            'body1'    => $this->body1,
+            'body2'    => $this->body2,
+            'body3'    => $this->body3,
+            'body4'    => $this->body4,
+            'body5'    => $this->body5,
         ];
     }
 }
