5e3ba0a0d407580d1dd71ef639a831ab239811fe
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-07 18:25:49 +0300

    an init

--- app/Http/Resources/Frontend/OurCertificateResource.php
+++ app/Http/Resources/Frontend/OurCertificateResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Frontend;
 
+use App\Models\CertificateCategory;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OurCertificateResource extends JsonResource
@@ -14,11 +15,65 @@ class OurCertificateResource extends JsonResource
      */
     public function toArray($request)
     {
+        foreach (CertificateCategory::where('cat_id', 1)->get() as $cat1) {
+            $certificates_1[] = [
+                'title'   => $cat1->title,
+                'image'   => $cat1->image() ? request()->root(). '/uploads/' . $cat1->image()->url : null,
+                'pdf'     => $cat1->pdf() ? request()->root(). '/uploads/' . $cat1->pdf()->url : null,
+            ];
+        }
+        
+        foreach (CertificateCategory::where('cat_id', 2)->get() as $cat2) {
+            $certificates_2[] = [
+                'title'   => $cat2->title,
+                'image'   => $cat2->image() ? request()->root(). '/uploads/' . $cat2->image()->url : null,
+                'pdf'     => $cat2->pdf() ? request()->root(). '/uploads/' . $cat2->pdf()->url : null,
+            ];
+        }
+
+        foreach (CertificateCategory::where('cat_id', 3)->get() as $cat3) {
+            $certificates_3[] = [
+                'title'   => $cat3->title,
+                'image'   => $cat3->image() ? request()->root(). '/uploads/' . $cat3->image()->url : null,
+                'pdf'     => $cat3->pdf() ? request()->root(). '/uploads/' . $cat3->pdf()->url : null,
+            ];
+        }
+
         return [
-            'id'      => $this->id,
-            'background' => ($this->image1) ? request()->root() . '/uploads/' . $this->image1->url : NULL,
-            ''
-            ''
+            'background1' => ($this->image1) ? request()->root() . '/uploads/' . $this->image1->url : NULL,
+            'bgTitle'    => $this->bgTitle1,
+            'bgSubTitle' => $this->bgSubTitle1,
+            'bgColor1'   => $this->bgColor1,
+            'body1'      => $this->body1,
+
+            'has_download'  => (boolean)$this->has_download,
+            'download_name' => $this->download_name,
+            'download_file' => ($this->image_pdf) ? request()->root() . '/uploads/' . $this->image_pdf->url : NULL,
+
+
+            'background2'  => ($this->image2) ? request()->root() . '/uploads/' . $this->image2->url : NULL,
+            'bgTitle2'     => $this->bgTitle2,
+            'bgSubTitle2'  => $this->bgSubTitle2,
+            'bgColor2'     => $this->bgColor2,
+            'body2'        => $this->body2,
+            'hint2'        => $this->hint2,
+
+            'certificates_1' => $certificates_1,
+            'certificates_2' => $certificates_2,
+            'certificates_3' => $certificates_3,
+
+
+            'duration'      => $this->duration,
+
+            'background3'  => ($this->image3) ? request()->root() . '/uploads/' . $this->image3->url : NULL,
+            'title3'       => $this->dTitle,
+
+            'background4'  => ($this->image4) ? request()->root() . '/uploads/' . $this->image4->url : NULL,
+            'title4'       => $this->cTitle,
+            'body4'        => $this->cBody,
+
+
+
         ];
     }
 }
