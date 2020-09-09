783cb92a9e3c2e0c77a18f00830dd2d9902cccc4
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-09 14:42:26 +0300

    an init

--- app/Http/Resources/Frontend/SettingResource.php
+++ app/Http/Resources/Frontend/SettingResource.php
@@ -15,9 +15,20 @@ class SearchResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'         => $this->id,
-            'pageSlug'   => $this->pageSlug,
-            'pageName'   => $this->pageName,
+            'id'            => $this->id,
+            'image'         => ($this->image) ? request()->root() .'/uploads/' . $this->image->url : NULL,
+            'image2'         => ($this->image2) ? request()->root() .'/uploads/' . $this->image2->url : NULL,
+            'bgTitle'       => $this->bgTitle,
+            'bgSubTitle'    => $this->bgSubTitle,
+            'bgColor'       => $this->bgColor,
+            'title'         => $this->title,
+
+            'body1'         => $this->body1,
+            'body2'         => $this->body2,
+            'body3'         => $this->body3,
+            'body4'         => $this->body4,
+            'body5'         => $this->body5,
+            'body6'         => $this->body6,
         ];
     }
 }
