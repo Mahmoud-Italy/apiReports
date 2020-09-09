7423701d0c7bbfcc3302a78c98c15eb1bb747a72
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-09 14:43:35 +0300

    an init

--- app/Http/Resources/Frontend/SettingResource.php
+++ app/Http/Resources/Frontend/SettingResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Frontend;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class SearchResource extends JsonResource
+class SettingResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
