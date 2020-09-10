3d181a3f5309308cb1738466d49580771ce9f419
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-10 17:28:26 +0300

    an init

--- app/Http/Resources/EmailTemplateResource.php
+++ app/Http/Resources/EmailTemplateResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class AboutResource extends JsonResource
+class EmailTemplateResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
