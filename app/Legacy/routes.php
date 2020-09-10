a2a6d1eb1fea663a4ae3267b2f93f62b3eef5c38
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-10 14:43:38 +0300

    an init

--- app/Legacy/routes.php
+++ app/Legacy/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Legacy;
 
 class Routes
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 use App\Legacy\Routes;
 $app = new Routes($router);
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
