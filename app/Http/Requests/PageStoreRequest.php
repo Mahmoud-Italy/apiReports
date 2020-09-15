936b9b5947f803c6eda3270fe19ed112ef965b7a
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-15 11:13:43 +0300

    an init

--- app/Http/Requests/PageStoreRequest.php
+++ app/Http/Requests/PageStoreRequest.php
@@ -25,7 +25,7 @@ class PageStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'image'  => 'mimes:jpeg,jpg,png,gif|max:10000', // max 10MB
+            //'image'  => 'mimes:jpeg,jpg,png,gif|max:10000', // max 10MB
             'slug'   => 'required|unique:pages,slug',
             'title'  => 'required'
         ];
