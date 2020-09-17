d7ad8661de80b4549c440b5c2e9d80ceae5e006e
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-09-17 11:41:52 +0300

    an init

--- app/Http/Resources/Frontend/OurCertificateResource.php
+++ app/Http/Resources/Frontend/OurCertificateResource.php
@@ -38,9 +38,9 @@ class OurCertificateResource extends JsonResource
             'body2'         => $this->body2,
             'hint2'         => $this->hint2,
 
-            'cat1_name'      => Setting::select('body1')->where('id', 6)->pluck('body1'),
-            'cat2_name'      => Setting::select('body2')->where('id', 6)->pluck('body2'),
-            'cat3_name'      => Setting::select('body3')->where('id', 6)->pluck('body3'),
+            'cat1_name'      => Setting::select('body1')->where('id', 6)->first(),
+            'cat2_name'      => Setting::select('body2')->where('id', 6)->first(),
+            'cat3_name'      => Setting::select('body3')->where('id', 6)->first(),
 
             'certificates_1' => CertificateCategoryResource::collection(CertificateCategory::where('cat_id', 1)->get()),
             'certificates_2' => CertificateCategoryResource::collection(CertificateCategory::where('cat_id', 2)->get()),
