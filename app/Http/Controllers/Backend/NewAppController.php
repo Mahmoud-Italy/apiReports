ce881cec7c50a43e8186ce8122532da97d6a652e
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-10-17 19:35:29 +0300

    an init

--- app/Http/Controllers/Backend/NewAppController.php
+++ app/Http/Controllers/Backend/NewAppController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Backend\NewAppResource;
 
-class NewAppAppController extends Controller
+class NewAppController extends Controller
 {
     function __construct()
     {
